84254fee988c7d4e19d98dfdeb87c1d6ac4861c2
DaronFox <daronfox95@gmail.com>
2018-09-02 21:42:38 +0200

    start making correct api for led controlling

--- led.py
+++ led.py
@@ -1,5 +1,46 @@
 from flask_restful import Resource
+from flask.views import MethodView
+import json
 
-class Test(Resource):
+# LED CONFIG
+LED_COUNT      = 16      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+#for testing
+class Color():
+    def __init__(self, red, green, blue):
+        self.red = red
+        self.green = green
+        self.blue = blue
+#/
+
+colors = {
+"red" : Color(255, 0, 0),
+"blue" : "blue",
+"green" : "green",
+"white" : "white"
+}
+
+class ChangeColor(Resource):
+    def get(self, color):
+        return {'color': colors[color]}
+
+class Brightness(Resource):
     def get(self):
-        return {'hello':'world'}
+        return {'some':'bright'}
+
+class SwitchOn(Resource):
+    def get(self):
+        return {'switch':"on"}
+
+class SwitchOff(Resource):
+    def get(self):
+        return {'switch':"off"}
+
+class Status(Resource):
+    def get(self):
+        return {'show':"status"}
--- run.py
+++ run.py
@@ -5,7 +5,11 @@ import led
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(led.Test, "/")
+api.add_resource(led.ChangeColor, "/changeColor/<string:color>")
+api.add_resource(led.Brightness, "/brightness/<int:percent>")
+api.add_resource(led.SwitchOn, "/switchOn")
+api.add_resource(led.SwitchOff, "/switchOff")
+api.add_resource(led.Status, "/status")
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=4040, debug=True);
